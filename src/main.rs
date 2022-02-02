2bcbe06d55f09df4b422e9eca32bcff325cbb739
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-02-02 11:12:27 +0100

    Empty map

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,28 @@ mod prelude {
 
 use prelude::*;
 
-struct State {}
+struct State {
+    map: Map,
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+        self.map.render(ctx)
     }
 }
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
+
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Dungeoncrawler")
+        .with_fps_cap(30.0)
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
