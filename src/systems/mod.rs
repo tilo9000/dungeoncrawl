8af8a92b575a5b262984ba533dc947368f4adf1e
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-02-15 16:38:37 +0100

    Monster fight back

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -23,9 +23,9 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(movement::movement_system())
-        .flush()
         .add_system(combat::combat_system())
+        .flush()
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
@@ -39,9 +39,9 @@ pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_moves::random_moves_system())
         .flush()
-        .add_system(movement::movement_system())
-        .flush()
         .add_system(combat::combat_system())
+        .flush()
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
--- src/systems/random_moves.rs
+++ src/systems/random_moves.rs
@@ -3,9 +3,12 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(MoveRandomly)]
+#[read_component(Health)]
+#[read_component(Player)]
 pub fn random_moves(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut movers = <(Entity, &Point, &MoveRandomly)>::query();
-    movers.iter_mut(ecs).for_each(|(entity, pos, _)| {
+    let mut positions = <(Entity, &Point, &Health)>::query();
+    movers.iter(ecs).for_each(|(entity, pos, _)| {
         let mut rng = RandomNumberGenerator::new();
         let destination = match rng.range(0, 4) {
             0 => Point::new(-1, 0),
@@ -13,12 +16,35 @@ pub fn random_moves(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
             2 => Point::new(0, -1),
             _ => Point::new(0, 1),
         } + *pos;
-        commands.push((
-            (),
-            WantsToMove {
-                entity: *entity,
-                destination,
-            },
-        ));
+        let mut attacked = false;
+        positions
+            .iter(ecs)
+            .filter(|(_, target_pos, _)| **target_pos == destination)
+            .for_each(|(victim, _, _)| {
+                if ecs
+                    .entry_ref(*victim)
+                    .unwrap()
+                    .get_component::<Player>()
+                    .is_ok()
+                {
+                    commands.push((
+                        (),
+                        WantsToAttack {
+                            attacker: *entity,
+                            victim: *victim,
+                        },
+                    ));
+                }
+                attacked = true;
+            });
+        if !attacked {
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+        }
     })
 }
