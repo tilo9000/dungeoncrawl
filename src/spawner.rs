823112eea882c3a0480cf0d3f2eb4a219c700866
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-02-16 11:33:32 +0100

    Chasing monsters

--- src/spawner.rs
+++ src/spawner.rs
@@ -9,8 +9,8 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             glyph: to_cp437('@'),
         },
         Health {
-            current: 20,
-            max: 20,
+            current: 10,
+            max: 10,
         },
     ));
 }
@@ -20,20 +20,37 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
         1..=8 => goblin(),
         _ => orc(),
     };
-    ecs.push((
-        Enemy,
-        pos,
-        Render {
-            color: ColorPair::new(WHITE, BLACK),
-            glyph,
-        },
-        MoveRandomly {},
-        Health {
-            current: hp,
-            max: hp,
-        },
-        Name(name),
-    ));
+    // 10% move randomly
+    match rng.roll_dice(1, 10) {
+        1..=9 => ecs.push((
+            Enemy,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph,
+            },
+            ChasingPlayer {},
+            Health {
+                current: hp,
+                max: hp,
+            },
+            Name(name),
+        )),
+        _ => ecs.push((
+            Enemy,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph,
+            },
+            MoveRandomly {},
+            Health {
+                current: hp,
+                max: hp,
+            },
+            Name(name),
+        )),
+    };
 }
 
 fn goblin() -> (i32, String, FontCharType) {
