d5f198cecf6b80ffb80a4c6f44ef606c97bf605c
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-02-17 15:52:38 +0100

    Victory

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -2,20 +2,28 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(Health)]
+#[read_component(Point)]
 #[read_component(Player)]
+#[read_component(AmuletOfYala)]
 pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
-    let mut player_hp = <&Health>::query().filter(component::<Player>());
+    let mut player = <(&Health, &Point)>::query().filter(component::<Player>());
+    let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
     let current_state = turn_state.clone();
+
+    let amulet_pos = amulet.iter(ecs).nth(0).unwrap();
 
     let mut new_state = match turn_state {
         TurnState::AwaitingInput => return,
         TurnState::PlayerTurn => TurnState::MonsterTurn,
         TurnState::MonsterTurn => TurnState::AwaitingInput,
-        TurnState::GameOver => current_state,
+        TurnState::GameOver | TurnState::Victory => current_state,
     };
-    player_hp.iter(ecs).for_each(|hp| {
+    player.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1 {
             new_state = TurnState::GameOver;
+        }
+        if pos == amulet_pos {
+            new_state = TurnState::Victory;
         }
     });
 
