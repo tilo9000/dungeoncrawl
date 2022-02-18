6d905207578f201cefa6a8141cefeba709aeda8f
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-02-18 11:38:53 +0100

    Player FOV

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -1,14 +1,20 @@
 use crate::prelude::*;
 
 #[system]
-pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {
+#[read_component(FieldOfView)]
+#[read_component(Player)]
+pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera) {
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(MAP_LAYER);
+
     for y in camera.top_y..camera.bottom_y {
         for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
-            if Map::in_bounds(pt) {
+            if Map::in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
                 let idx = get_idx(x, y);
 
                 let glyph = match map.tiles.get(idx).unwrap() {
--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 #[allow(clippy::trivially_copy_pass_by_ref)]
 #[system(for_each)]
 #[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn movement(
     entity: &Entity,
     want_move: &WantsToMove,
@@ -12,14 +13,14 @@ pub fn movement(
 ) {
     if map.can_enter_tile(want_move.destination) {
         commands.add_component(want_move.entity, want_move.destination);
+        if let Ok(entry) = ecs.entry_ref(want_move.entity) {
+            if let Ok(fov) = entry.get_component::<FieldOfView>() {
+                commands.add_component(want_move.entity, fov.clone_dirty());
+            }
 
-        if ecs
-            .entry_ref(want_move.entity)
-            .unwrap()
-            .get_component::<Player>()
-            .is_ok()
-        {
-            camera.on_player_move(want_move.destination);
+            if entry.get_component::<Player>().is_ok() {
+                camera.on_player_move(want_move.destination);
+            }
         }
     }
     commands.remove(*entity);
