72be1fca234a453b08234fabb83d152780123ff4
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-02-21 15:02:06 +0100

    Spatial memory

--- src/map.rs
+++ src/map.rs
@@ -10,6 +10,7 @@ pub enum TileType {
 
 pub struct Map {
     pub tiles: Vec<TileType>,
+    pub revealed_tiles: Vec<bool>,
 }
 
 #[allow(clippy::cast_sign_loss)]
@@ -21,6 +22,7 @@ impl Map {
     pub fn new() -> Self {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
+            revealed_tiles: vec![false; NUM_TILES],
         }
     }
 
