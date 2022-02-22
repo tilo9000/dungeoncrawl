bb888f1ea8f2dd64a346f94e49d9a8a84f961a53
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-02-22 16:43:28 +0100

    Add architect for room maps

--- src/map_builder/rooms.rs
+++ src/map_builder/rooms.rs
@@ -1,9 +1,9 @@
 use super::MapArchitect;
 use crate::prelude::*;
 
-pub struct EmptyArchitect {}
+pub struct RoomsArchitect {}
 
-impl MapArchitect for EmptyArchitect {
+impl MapArchitect for RoomsArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder {
         let mut mb = MapBuilder {
             map: Map::new(),
@@ -12,17 +12,14 @@ impl MapArchitect for EmptyArchitect {
             player_start: Point::zero(),
             amulet_start: Point::zero(),
         };
-        mb.fill(TileType::Floor);
-        mb.player_start = Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+        mb.fill(TileType::Wall);
+        mb.build_random_rooms(rng);
+        mb.build_corridors(rng);
+        mb.player_start = mb.rooms[0].center();
         mb.amulet_start = mb.find_most_distant();
-
-        for _ in 0..50 {
-            mb.monster_spawns.push(Point::new(
-                rng.range(1, SCREEN_WIDTH),
-                rng.range(1, SCREEN_HEIGHT),
-            ));
+        for room in mb.rooms.iter().skip(1) {
+            mb.monster_spawns.push(room.center());
         }
-
         mb
     }
 }
