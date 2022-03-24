221e24077b9b188d30cc3739855e9304dd0a2ca4
Tilo Reinhardt <tilo.reinhardt@gmail.com>
2022-03-24 11:26:55 +0100

    Random map selection

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -26,8 +26,14 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = DrunkardsWalkArchitect {};
-        architect.new(rng)
+        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect {}),
+            1 => Box::new(RoomsArchitect {}),
+            _ => Box::new(CellularAutomataArchitect {}),
+        };
+        let mut mb = architect.new(rng);
+
+        mb
     }
 
     fn fill(&mut self, tile: TileType) {
